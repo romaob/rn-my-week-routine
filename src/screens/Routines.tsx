f88c0b99b2294bba52ce5c1c8e846d7f3022cc9a
Romao Barbosa <57117334+romaob@users.noreply.github.com>
2023-09-05 22:24:46 -0300

    Added features and improvements in the app: (#18)
    - Implemented the Drawer menu, with the empty screens
    - Implemented a back button added on screens where the drawer is not available
    - Updated navigation color options and styles

---
+++ src/screens/Routines.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function Routines() {
+  return (
+    <View>
+      <Text>List of events created</Text>
+     </View>
+  );
+}
