ba6499f87427a08beb9c32cfc7919d802f24ac68
Romao Barbosa <57117334+romaob@users.noreply.github.com>
2023-09-06 11:15:04 -0300

    Feature: Event list screen (#19)
    Summary of changes:
    - Implemented the Event List screen
    - Created the Event list item component
    - Created tests for the Event list item component
    - Updated Label, RoutineList and WeekDaysMenu components
    - Updated Home screen
    - Added new strings
    - Updated babel.config.js to fix FlatList issues, for more info see: https://github.com/facebook/react-native/issues/36828#issuecomment-1589462227
    - Updated package.json to fix FlatList issues.
    - Updated jest.setup.js to support the navigation Drawer

--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,21 @@
+// include this line for mocking react-native-gesture-handler
+import 'react-native-gesture-handler/jestSetup';
+
 //AsyncStorage mock
 jest.mock('@react-native-async-storage/async-storage', () =>
   require('@react-native-async-storage/async-storage/jest/async-storage-mock'),
 );
 
+// Mocking react-native-reanimated
+jest.mock('react-native-reanimated', () => {
+  const Reanimated = require('react-native-reanimated/mock');
+
+  // The mock for `call` immediately calls the callback which is incorrect
+  // So we override it with a no-op
+  Reanimated.default.call = () => {};
+
+  return Reanimated;
+});
+
+// Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
---
+++ src/components/EventListItem.tsx
@@ -0,0 +1,144 @@
+import React from 'react';
+import {getTimeStringFromDate, getWeekDays} from '../utils/dateUtils';
+import {StyleSheet, View} from 'react-native';
+import Label, {FontSize} from './Label';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import {Event} from '../values/appDefaults';
+import {colors} from '../values/colors';
+import {sizes} from '../values/sizes';
+import useString from '../hooks/useString';
+
+function WeekDaysListItem({
+  day,
+  selected,
+}: {
+  day: string;
+  selected: boolean;
+}): JSX.Element {
+  return (
+    <View
+      style={{
+        ...styles.weekDaysListItem,
+        ...(selected ? styles.weekDaysListItemSelected : {}),
+      }}
+      testID="eventListItemWeekDay">
+      <Label text={day.charAt(0).toUpperCase()} size={FontSize.SMALL} />
+    </View>
+  );
+}
+
+export default function EventListItem({
+  item,
+  onLongPress,
+}: {
+  item: Event;
+  onLongPress?: (event: Event) => void;
+}): JSX.Element {
+  const {language, getString} = useString();
+  const weekDays = getWeekDays(language);
+
+  return (
+    <TouchableOpacity
+      testID="eventListItem"
+      style={styles.listItem}
+      onLongPress={() => onLongPress && onLongPress(item)}>
+      <Label
+        text={item.name}
+        style={styles.title}
+        color={colors.light.accent}
+        testID="eventListItemName"
+      />
+      <Label
+        text={item.description}
+        size={FontSize.MEDIUM}
+        color={colors.light.textSecondary}
+        testID="eventListItemDescription"
+      />
+      <View style={styles.listItemTimes}>
+        <Label
+          text={
+            getString('routine_start_at') +
+            ' ' +
+            getTimeStringFromDate(new Date(item.startAt))
+          }
+          size={FontSize.SMALL}
+          color={colors.light.textSecondary}
+          style={styles.listItemTimesText}
+          testID="eventListItemStart"
+        />
+        <Label
+          text={
+            getString('routine_end_at') +
+            ' ' +
+            getTimeStringFromDate(new Date(item.endAt))
+          }
+          size={FontSize.SMALL}
+          color={colors.light.textSecondary}
+          style={styles.listItemTimesText}
+          testID="eventListItemEnd"
+        />
+      </View>
+      <View style={styles.listItemFooter}>
+        {weekDays.map((day, index) => (
+          <WeekDaysListItem
+            key={index}
+            day={day}
+            selected={item.indexes.includes(index)}
+          />
+        ))}
+      </View>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.light.background,
+    paddingVertical: sizes.padding.sm,
+  },
+  listItem: {
+    display: 'flex',
+    margin: sizes.margin.sm,
+    padding: sizes.padding.sm,
+    minHeight: 40,
+    backgroundColor: colors.light.cardBackground,
+    gap: sizes.margin.sm,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: colors.light.grey + '70',
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+  listItemTimes: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  listItemTimesText: {
+    fontWeight: 'bold',
+    flex: 1,
+  },
+  listItemFooter: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+  },
+  weekDaysListItem: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: sizes.margin.sm,
+    borderRadius: 999,
+    padding: 3,
+    width: 25,
+    height: 25,
+    backgroundColor: colors.light.grey + '30',
+    borderWidth: 1,
+    borderColor: colors.light.grey + '50',
+  },
+  weekDaysListItemSelected: {
+    backgroundColor: colors.light.accent + '70',
+  },
+});
